2e4294c90486f9cd307917be033e148fbe18c0d6
sakifukumoto <saki.fukumoto9918@gmail.com>
2023-10-15 20:40:37 +0900

    基本操作

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-require('dotenv').config();
+// const { Client, GatewayIntentBits, MessageActionRow, MessageButton } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config()
 
-client.on('ready', () => {
-  console.log('起動完了');
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages
+  ]
 });
 
-client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('Pong!');
+client.once('ready', () => {
+  console.log('Ready!') // 起動した時に"Ready!"とコンソールに出力する
+});
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+  const textChannel = newState.guild.channels.cache.get('1162959363409973278')
+  if (oldState.channelId === null && newState.channelId !== null) {
+    textChannel.send('ボイスチャンネルに参加しました!')
+  } else if (oldState.channelId !== null && newState.channelId === null) {
+    textChannel.send('ボイスチャンネルから退出しました!')
   }
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.on('messageCreate', message => {
+  const textChannel = message.guild.channels.cache.get('1162959363409973278')
+  if (message.content === '!schedule') {
+    textChannel.send('メッセージが送信されました')
+  }
+});
+
+client.login(DISCORD_TOKEN);
